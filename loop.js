d0bb577a4136fcb7a38263888346fd6b0c0ed5e0
lfgama <filipegama@live.com>
2020-11-17 19:14:05 -0300

    soma.js

---
+++ loop.js
@@ -0,0 +1,8 @@
+let numeros = [1, 2, 3, 4, 5];
+function encontreUm5 (numeros) {
+    do { 
+        console.log(numeros);
+        numeros++;
+    } while (numeros != 5)
+    console.log("Encontramos um 5!")
+}
---
+++ setdate.js
@@ -0,0 +1,6 @@
+let data = new Date ()
+data.setDate(19);
+data.setMonth(3);
+data.setFullYear(1991);
+console.log(data);
+console.log("commit")
